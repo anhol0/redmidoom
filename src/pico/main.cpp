89c8b0c608a5314feaec40a0cd484ccca42590fc
anhol <holo.linuxlv@gmail.com>
2026-09-20 15:17:26 -0400

    Update

--- src/pico/main.cpp
+++ src/pico/main.cpp
@@ -3,7 +3,7 @@
 // ethernet module. Then it will display the output to the STM7789V based
 // 320x240 display. Input polling will be performed through a set of buttons
 // connected to GPIO pins on the Pico. Display and Ethernet controllers will be
-// connected to the SPI0 and SPI1 respectively. This is done to prevent fighting
+// connected to the SPI1 and SPI0 respectively. This is done to prevent fighting
 // over the SPI bus. Pico side implementation will be written in C++ using Pico
 // SDK, while AC2100 side implementation will be written in C using Libc
 // provided by the OpenWRT 25.12.2 system
