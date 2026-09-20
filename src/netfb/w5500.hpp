f1dfad152a1b7243b7cfcfb38ad9eb3e09e7a9a7
anhol <holo.linuxlv@gmail.com>
2026-09-20 15:17:14 -0400

    Add all the basic files

---
+++ src/netfb/w5500.hpp
@@ -0,0 +1,3 @@
+// This file will contain function definitions and constants for controlling
+// W5500 ethernet module. It will be used to transmit data between the AC2100
+// and Pico
---
+++ src/pico/main.cpp
@@ -0,0 +1,9 @@
+// Here will be code for Raspberry Pi Pico side implementation of redmidoom
+// project. Pico will be used for polling data from the router through W5500
+// ethernet module. Then it will display the output to the STM7789V based
+// 320x240 display. Input polling will be performed through a set of buttons
+// connected to GPIO pins on the Pico. Display and Ethernet controllers will be
+// connected to the SPI0 and SPI1 respectively. This is done to prevent fighting
+// over the SPI bus. Pico side implementation will be written in C++ using Pico
+// SDK, while AC2100 side implementation will be written in C using Libc
+// provided by the OpenWRT 25.12.2 system
